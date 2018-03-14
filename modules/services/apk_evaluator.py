3adc60db5f9b2dc32589880373f525cc455a2e87
Arstsiom Kushniarou <kushnerovartem@gmail.com>
2018-03-14 13:26:46 +0100

    Make criteria a field of the ApkEvaluator

--- modules/services/apk_evaluator.py
+++ modules/services/apk_evaluator.py
@@ -3,50 +3,53 @@ from datetime import datetime
 
 class ApkEvaluator:
 
-    date_pattern = '%Y-%m-%d %H:%M:%S'
+    DATE_PATTERN = '%Y-%m-%d %H:%M:%S'
 
-    def satisfies(self, apk, criteria):
+    def __init__(self, criteria=None):
+        self.criteria = criteria
+
+    def satisfies(self, apk):
         if not apk:
             return False
-        return self.satisfies_markets(apk, criteria) and self.satisfies_size(apk, criteria) and self.satisfies_date(apk, criteria) and self.satisfies_name(apk, criteria) and self.satisfies_vt_detection(apk, criteria)
+        return self.satisfies_markets(apk) and self.satisfies_size(apk) and self.satisfies_date(apk) and self.satisfies_name(apk) and self.satisfies_vt_detection(apk)
 
-    def satisfies_markets(self, apk, criteria):
+    def satisfies_markets(self, apk):
         satisfies = True
-        criteria_markets = criteria.get('markets')
+        criteria_markets = self.criteria.get('markets')
         if criteria_markets:
             satisfies = apk.markets and criteria_markets.intersection(set(apk.markets))
         return satisfies
 
-    def satisfies_vt_detection(self, apk, criteria):
+    def satisfies_vt_detection(self, apk):
         satisfies = True
-        criteria_vt_detection = criteria.get('vt_detection')
+        criteria_vt_detection = self.criteria.get('vt_detection')
         if criteria_vt_detection:
             satisfies = self.falls_into_range(apk.vt_detection, criteria_vt_detection)
         return satisfies
 
-    def satisfies_name(self, apk, criteria):
+    def satisfies_name(self, apk):
         satisfies = True
-        criteria_pkg_name = criteria.get('pkg_name')
+        criteria_pkg_name = self.criteria.get('pkg_name')
         if criteria_pkg_name:
             satisfies = apk.pkg_name in criteria_pkg_name
         return satisfies
 
-    def satisfies_size(self, apk, criteria):
+    def satisfies_size(self, apk):
         satisfies = True
-        criteria_apk_size = criteria.get('apk_size')
+        criteria_apk_size = self.criteria.get('apk_size')
         if criteria_apk_size:
             satisfies = self.falls_into_range(apk.apk_size, criteria_apk_size)
         return satisfies
 
-    def satisfies_date(self, apk, criteria):
+    def satisfies_date(self, apk):
         satisfies = True
-        criteria_dex_date = criteria.get('dex_date')
-        apk_date = datetime.strptime(apk.dex_date, self.date_pattern) if apk.dex_date else None
+        criteria_dex_date = self.criteria.get('dex_date')
+        apk_date = datetime.strptime(apk.dex_date, self.DATE_PATTERN) if apk.dex_date else None
         if criteria_dex_date:
             satisfies = self.falls_into_range(apk_date, criteria_dex_date)
 
         return satisfies
-    
+
     def falls_into_range(self, value, bounds):
         satisfies = True
         lower = bounds.get('from')
--- modules/services/dataset_factory.py
+++ modules/services/dataset_factory.py
@@ -6,9 +6,10 @@ from modules.services.apk_evaluator import ApkEvaluator
 
 
 class DatasetFactory:
-    def __init__(self, source, evaluator=ApkEvaluator()):
+    def __init__(self, source, criteria, evaluator=ApkEvaluator()):
         self.source = source
         self.apk_evaluator = evaluator
+        self.apk_evaluator.criteria = criteria
 
     def create_dataset(self, criteria):
         logging.info(f'CREATE INITIAL DATASET BY CRITERIA: {criteria}')
@@ -16,7 +17,7 @@ class DatasetFactory:
         dataset = Dataset()
         for apk in self.source:
             logging.debug(f'EVALUATE {apk.pkg_name}')
-            if self.apk_evaluator.satisfies(apk, criteria):
+            if self.apk_evaluator.satisfies(apk):
                 logging.debug(f'{str(apk)} SATISFIES CRITERIA' )
                 dataset.add(apk)
         logging.info(f'DATASET HAS BEEN CREATED. {len(dataset)} APKS SATISFY CRITERIA')

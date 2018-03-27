72e0801b8f4db4c1c07d492c03b4f9bbb010f432
artsiom.kushniarou <kushnerovartem@gmail.com>
2018-03-27 16:32:51 +0200

    Add documentation

--- cli.py
+++ cli.py
@@ -10,17 +10,26 @@ from modules.entities.criteria import Criteria
 
 
 @click.command()
-@click.option('--number', '-n', type=click.INT, help='Number of apks to download')
-@click.option('--dexdate', '-d', help='Dex date in format %Y-%m-%d')
-@click.option('--apksize', '-s', type=click.INT, help='Apk size TODO: in what units')
-@click.option('--vtdetection', '-vt', type=click.INT, help='Virus total rating')
+@click.option('--number', '-n', type=click.INT, help='Number of apks to download.')
+@click.option('--dexdate', '-d', help='The date on a dex file, format %Y-%m-%d, e.g. 2015-10-03')
+@click.option('--apksize', '-s', help='Apk size, in bytes')
+@click.option('--vtdetection', '-vt', help='Virus total rating, integer')
 @click.option('--pkgname', '-pn', help='Package names')
-@click.option('--markets', '-m', help='Markets')
-@click.option('--metadata', '-md', help='Metadata')
-@click.option('--out', '-o', help='Output folder name')
+@click.option('--markets', '-m', help='Markets, e.g. play.google.com')
+@click.option('--metadata', '-md', help='Metadata. This is a subset of latest.csv column names to keep in metadata.csv. By default sha256,pkg_name,apk_size,dex_date,markets')
+@click.option('--out', '-o', help='Output folder name. By default current directory')
 @click.option('--seed', '-sd', type=click.INT, help='Seed for a random algorithm')
 def run(number, dexdate, apksize, vtdetection, pkgname, markets, metadata, out, seed):
-    """Downloads specified number of apks satisfying specified criteria from androzoo repository. Use ':' as a delimiter for range options and ',' for list options"""
+    """Downloads specified number of apks satisfying specified criteria from androzoo repository. Saves specified metadata to metadata.csv.
+    dexdate, apksize and vtdetection require specifying lower and upper bounds in format lower:upper, both inclusive. One of the bounds can be omitted (i.e. you can write :upper or lower:)
+    pkgname, markets, metadata can be either single values or comma separated lists.
+
+    Sample usage:
+
+    az -n 10 -d 2015-12-11: -s :3000000  -m play.google.com,appchina
+
+    This means: download 10 apks with the dexdate starting from the 2015-12-11(inclusive), size up to 3000000 bytes(inclusive) and present on either play.google.com or appchina
+     """
     logging_util.setup_logging()
     *criteria_args, metadata = Parser(dexdate, apksize, vtdetection, markets, pkgname, metadata).parse()
     criteria = Criteria(*criteria_args)

620c0c079eb99e0c21fa8ac1c73b9074f78d1ed1
Arstsiom Kushniarou <kushnerovartem@gmail.com>
2018-05-21 12:28:27 +0200

    Upper case input hashes

--- modules/cli/parser.py
+++ modules/cli/parser.py
@@ -25,8 +25,11 @@ class Parser:
         vt_detection_from, vt_detection_to = self.vtdetection.split(self.RANGE_ARGS_DELIMITER) if self.vtdetection else (None, None)
         markets = self.markets.split(self.LIST_ARGS_DELIMITER) if self.markets else None
         pkg_name = self.pkgname.split(self.LIST_ARGS_DELIMITER) if self.pkgname else None
-        sha256 = self.sha256.split(self.LIST_ARGS_DELIMITER) if self.sha256 else None
-        sha1 = self.sha1.split(self.LIST_ARGS_DELIMITER) if self.sha1 else None
-        md5 = self.md5.split(self.LIST_ARGS_DELIMITER) if self.md5 else None
+        sha256 = self.get_hash_list(self.sha256) if self.sha256 else None
+        sha1 = self.get_hash_list(self.sha1) if self.sha1 else None
+        md5 = self.get_hash_list(self.md5) if self.md5 else None
         metadata = self.metadata.split(self.LIST_ARGS_DELIMITER) if self.metadata else self.METADATA_DEFAULT_VALUE
         return number, dex_date_from, dex_date_to, apksize_from, apksize_to, vt_detection_from, vt_detection_to, markets, pkg_name, sha256, sha1, md5, metadata
+
+    def get_hash_list(self, apk_hashes):
+        return [apk_hash.upper() for apk_hash in apk_hashes.split(self.LIST_ARGS_DELIMITER)]

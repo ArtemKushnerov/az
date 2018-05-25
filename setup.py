0555060c6cfa651df51a02e4cdb833280d554dc4
Artsiom Kushniarou <kushnerovartem@gmail.com>
2018-05-25 15:06:50 +0200

    - provide --sha256, --sha1, --md5 options - add --version option - prohibit executing without arguments (show help instead)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='az',
-    version='1.0.1',
+    version='1.1.0',
     entry_points={
         'console_scripts': [
             'az=main:run'

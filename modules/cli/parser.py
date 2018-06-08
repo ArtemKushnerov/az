be639f371efe0cdf0171ff204da5e10e4e895781
Arstsiom Kushniarou <kushnerovartem@gmail.com>
2018-06-08 11:11:46 +0200

    Improv config: add cli options, implement sequential config reading

--- modules/cli/parser.py
+++ modules/cli/parser.py
@@ -1,3 +1,4 @@
+from modules.cli.user_config import UserConfig
 from modules.enums import DownloadType
 
 
@@ -6,30 +7,27 @@ class Parser:
     LIST_ARGS_DELIMITER = ','
     METADATA_DEFAULT_VALUE = ['sha256', 'pkg_name', 'apk_size', 'dex_date', 'markets']
 
-    def __init__(self, number, dexdate, apksize, vtdetection, markets, pkgname, metadata, sha256, sha1, md5):
-        self.number = number
-        self.dexdate = dexdate
-        self.apksize = apksize
-        self.vtdetection = vtdetection
-        self.markets = markets
-        self.pkgname = pkgname
-        self.metadata = metadata
-        self.sha256 = sha256
-        self.sha1 = sha1
-        self.md5 = md5
+    def __init__(self, args):
+        self.args = args
 
     def parse(self):
-        number = int(self.number) if self.number else DownloadType.ALL
-        dex_date_from, dex_date_to = self.dexdate.split(self.RANGE_ARGS_DELIMITER) if self.dexdate else (None, None)
-        apksize_from, apksize_to = self.apksize.split(self.RANGE_ARGS_DELIMITER) if self.apksize else (None, None)
-        vt_detection_from, vt_detection_to = self.vtdetection.split(self.RANGE_ARGS_DELIMITER) if self.vtdetection else (None, None)
-        markets = self.markets.split(self.LIST_ARGS_DELIMITER) if self.markets else None
-        pkg_name = self.pkgname.split(self.LIST_ARGS_DELIMITER) if self.pkgname else None
-        sha256 = self.get_hash_list(self.sha256) if self.sha256 else None
-        sha1 = self.get_hash_list(self.sha1) if self.sha1 else None
-        md5 = self.get_hash_list(self.md5) if self.md5 else None
-        metadata = self.metadata.split(self.LIST_ARGS_DELIMITER) if self.metadata else self.METADATA_DEFAULT_VALUE
-        return number, dex_date_from, dex_date_to, apksize_from, apksize_to, vt_detection_from, vt_detection_to, markets, pkg_name, sha256, sha1, md5, metadata
+        number = int(self.args.number) if self.args.number else DownloadType.ALL
+        dex_date_from, dex_date_to = self.args.dexdate.split(self.RANGE_ARGS_DELIMITER) if self.args.dexdate else (None, None)
+        apksize_from, apksize_to = self.args.apksize.split(self.RANGE_ARGS_DELIMITER) if self.args.apksize else (None, None)
+        vt_detection_from, vt_detection_to = self.args.vtdetection.split(self.RANGE_ARGS_DELIMITER) if self.args.vtdetection else (None, None)
+        markets = self.args.markets.split(self.LIST_ARGS_DELIMITER) if self.args.markets else None
+        pkg_name = self.args.pkgname.split(self.LIST_ARGS_DELIMITER) if self.args.pkgname else None
+        sha256 = self.get_hash_list(self.args.sha256) if self.args.sha256 else None
+        sha1 = self.get_hash_list(self.args.sha1) if self.args.sha1 else None
+        md5 = self.get_hash_list(self.args.md5) if self.args.md5 else None
+        metadata = self.args.metadata.split(self.LIST_ARGS_DELIMITER) if self.args.metadata else self.METADATA_DEFAULT_VALUE
+        key, input_file = self.args.key, self.args.input_file
+        if not key or not input_file:
+            user_config = UserConfig()
+            input_file = input_file if input_file else user_config.in_file
+            key = key if key else user_config.key
+
+        return number, dex_date_from, dex_date_to, apksize_from, apksize_to, vt_detection_from, vt_detection_to, markets, pkg_name, sha256, sha1, md5, metadata, key, input_file
 
     def get_hash_list(self, apk_hashes):
         return [apk_hash.upper() for apk_hash in apk_hashes.split(self.LIST_ARGS_DELIMITER)]
--- modules/cli/validator.py
+++ modules/cli/validator.py
@@ -3,34 +3,25 @@ from modules.exceptions import NoArgsException
 
 
 class Validator:
-    def __init__(self, number=None, dexdate=None, apksize=None, vtdetection=None, markets=None, pkgname=None, metadata=None, sha256=None, sha1=None, md5=None):
-        self.number = number
-        self.dexdate = dexdate
-        self.apksize = apksize
-        self.vtdetection = vtdetection
-        self.markets = markets
-        self.pkgname = pkgname
-        self.metadata = metadata
-        self.sha256 = sha256
-        self.sha1 = sha1
-        self.md5 = md5
+    def __init__(self, args):
+        self.args = args
 
     def validate(self):
-        self.check_at_least_one_arg_provided()
-        self.check_number_is_positive()
+        self._check_at_least_one_arg_provided()
+        self._check_number_is_positive()
         self._check_only_one_hash_is_used()
 
-    def check_at_least_one_arg_provided(self):
+    def _check_at_least_one_arg_provided(self):
         if all(value is None for value in vars(self).values()):
             raise NoArgsException()
 
     def _check_only_one_hash_is_used(self):
-        if (self.sha256 and self.sha1) or (self.sha256 and self.md5) or (self.sha1 and self.md5):
+        if (self.args.sha256 and self.args.sha1) or (self.args.sha256 and self.args.md5) or (self.args.sha1 and self.args.md5):
             raise ValidationException('Please specify only one hashing algorithm')
 
-    def check_number_is_positive(self):
+    def _check_number_is_positive(self):
         # None means download all
-        if not (self.number is None or self.number > 0):
+        if not (self.args.number is None or self.args.number > 0):
             raise ValidationException('Number of apks must be positive')
 
 

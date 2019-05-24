c80a196be1c13329818e25238b1dfef850a56c54
Artsiom Kushniarou <kushnerovartem@gmail.com>
2019-05-24 13:40:42 +0300

    Add additional fields to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+        long_description = fh.read()
 
 setup(
     name='az',
     version='1.2.2',
+    author="Artsiom Kushnariou",
+    author_email="kushnerovartem@gmail.com",
+    description="Downloads apks from androzoo repository https://androzoo.uni.lu/",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/ArtemKushnerov/az',
     entry_points={
         'console_scripts': [
             'az=modules.main:run'
@@ -19,5 +27,10 @@ setup(
         'python_dateutil==2.7.2',
         'PyYAML==4.2b1',
         'requests==2.20.0',
-    ]
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
